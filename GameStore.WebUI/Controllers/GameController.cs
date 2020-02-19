e74a7f69102f8c9424e817522db4898792b83439
Nar <Beaviself@gmail.com>
2020-02-19 03:08:46 +0300

    Pull the project

---
+++ GameStore.WebUI/Controllers/GameController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using GameStore.WebUI.Models;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class GameController : Controller
+    {
+        private IGameRepository repository;
+        public int pageSize = 4; // на одной странице должны отображаться сведения о 4 товарах.
+
+        public GameController(IGameRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List(string category, int page = 1) //int page = 1 - необязательный параметр. Это означает, что в случае вызова метода без параметра List() вызов обрабатывается так, словно ему было передано значение, указанное в определении параметра по умолчанию List(1). В результате метод действия отображает первую страницу сведений о товарах, когда MVC Framework вызывает его без аргумента.
+        {
+            GamesListViewModel model = new GamesListViewModel
+            {
+                Games = repository.Games
+                    .Where(p => category == null || p.Category == category)
+                    .OrderBy(game => game.GameId)
+                    .Skip((page - 1) * pageSize)
+                    .Take(pageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = pageSize,
+                    TotalItems = category == null ?
+                repository.Games.Count() :
+                repository.Games.Where(game => game.Category == category).Count()
+                },
+                CurrentCategory = category
+            };
+            return View(model);
+        }
+
+        public FileContentResult GetImage(int gameId)
+        {
+            Game game = repository.Games
+                .FirstOrDefault(g => g.GameId == gameId);
+
+            if (game != null)
+            {
+                return File(game.ImageData, game.ImageMimeType);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
